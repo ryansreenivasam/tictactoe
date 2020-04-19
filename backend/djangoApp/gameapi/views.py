729009515657913b3b45b4ea0db7ae207db2144b
ryansreenivasam <ryansreenivasam@gmail.com>
2020-04-18 20:05:40 -0500

    created view to convert game data to JSON format

--- backend/djangoApp/gameapi/views.py
+++ backend/djangoApp/gameapi/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Game
+from .serializers import GameSerializer
 
-# Create your views here.
+# This class will use the serializer to render data in JSON format.
+# The queryset will query the database for all game objects then pass that data
+# to the serializer where it is converted to JSON
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
