9ca527a8ea1b73272614c3652519ae29e83e57c1
ryansreenivasam <ryansreenivasam@gmail.com>
2020-04-20 21:40:30 -0500

    set up update game and buttons to allow any player move

--- spa/src/components/HelloWorld.vue
+++ spa/src/components/HelloWorld.vue
@@ -1,39 +1,62 @@
 <template>
   <div class="hello">
     <h1> Tic Tac Toe </h1>
+    <!--
+      This table element is the tic tac toe board.  Each place on the board is 
+      a button and when the button is pressed the updateGame method is called 
+      with the index of that button.
+    -->
     <div class="board">
       <table>
         <tr>
           <td>
-            <button class="boardButton" v-on:click="updateGame"> {{ gameBoard.board.substr(0,1) }} </button>
+            <button class="boardButton" v-on:click="updateGame(0)"> 
+              {{ gameBoard.board.substr(0,1) }} 
+            </button>
           </td>
           <td>
-            <button class="boardButton" v-on:click="updateGame"> {{ gameBoard.board.substr(1,1) }} </button>
+            <button class="boardButton" v-on:click="updateGame(1)"> 
+              {{ gameBoard.board.substr(1,1) }} 
+            </button>
           </td>
           <td>
-            <button class="boardButton" v-on:click="updateGame"> {{ gameBoard.board.substr(2,1) }} </button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button class="boardButton" v-on:click="updateGame"> {{ gameBoard.board.substr(3,1) }} </button>
-          </td>
-          <td>
-            <button class="boardButton" v-on:click="updateGame"> {{ gameBoard.board.substr(4,1) }} </button>
-          </td>
-          <td>
-            <button class="boardButton" v-on:click="updateGame"> {{ gameBoard.board.substr(5,1) }} </button>
+            <button class="boardButton" v-on:click="updateGame(2)"> 
+              {{ gameBoard.board.substr(2,1) }} 
+            </button>
           </td>
         </tr>
         <tr>
           <td>
-            <button class="boardButton" v-on:click="updateGame"> {{ gameBoard.board.substr(6,1) }} </button>
+            <button class="boardButton" v-on:click="updateGame(3)"> 
+              {{ gameBoard.board.substr(3,1) }} 
+            </button>
           </td>
           <td>
-            <button class="boardButton" v-on:click="updateGame"> {{ gameBoard.board.substr(7,1) }} </button>
+            <button class="boardButton" v-on:click="updateGame(4)"> 
+              {{ gameBoard.board.substr(4,1) }} 
+            </button>
           </td>
           <td>
-            <button class="boardButton" v-on:click="updateGame"> {{ gameBoard.board.substr(8,1) }} </button>
+            <button class="boardButton" v-on:click="updateGame(5)"> 
+              {{ gameBoard.board.substr(5,1) }} 
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button class="boardButton" v-on:click="updateGame(6)"> 
+              {{ gameBoard.board.substr(6,1) }} 
+            </button>
+          </td>
+          <td>
+            <button class="boardButton" v-on:click="updateGame(7)"> 
+              {{ gameBoard.board.substr(7,1) }} 
+            </button>
+          </td>
+          <td>
+            <button class="boardButton" v-on:click="updateGame(8)"> 
+              {{ gameBoard.board.substr(8,1) }} 
+            </button>
           </td>
         </tr>
       </table>
@@ -42,9 +65,6 @@
     <!-- This displays the current contents of the board as it is updated -->
     <h3>currentgame: {{ gameBoard }}</h3>
 
-    <!-- This is a button that sends a PUT request with current game board -->
-    <button v-on:click="updateGame"> UpdateGame </button>
-    
     <!-- This is a button that sends a POST request with an empty game board -->
     <button v-on:click="newGame"> NewGame </button>
   
@@ -52,16 +72,17 @@
 </template>
 
 <script>
-import axios from "axios"; //Axios is a tool to handle HTTP requests
+//Axios is a tool to handle HTTP requests
+import axios from "axios"; 
 
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String, // from default Vue app
   },
   data() {
     return {
-      gameBoard: {'board': '_________'} // Contents of game throughout play
+      // Contents of game throughout play
+      gameBoard: {'board': '_________'}
     }
   },
   methods: {
@@ -80,13 +101,21 @@ export default {
         });
     },
 
-    /* This method will update the current game object with a user's new move
-    and set response to true signaling that a new AI move has been requested.
-    The PUT request response will contain the new AI move.
+    /* This method will update the board with a user's new move.  The index in
+    the board string that corresponds to the placement of the user's move is 
+    updated with an X.  The game object's response field is set to true 
+    signaling that a new AI move has been requested. A PUT request is then sent to
+    the server with the updated information. The PUT request response will contain 
+    the new AI move.
     */
-    updateGame: function () {
-      var board = {'board': '____X____',  'response': true};
-      // This updates the current game with the new player move
+    updateGame: function (index) {
+      // Strings in Javascript are immutable so a copy must be made
+      var currBoard = this.gameBoard.board;
+      // Add X to the string in the index that the user requested
+      currBoard = currBoard.substring(0, index) + "X" + currBoard.substring(index+1);
+      // Create a board object with the new string and response set to true
+      var board = { 'board': currBoard, 'response': true};
+      // Send a PUT request with the new player move and request for an AI move
       axios.put(`http://127.0.0.1:8000/api/${this.gameBoard.id}/`, board) 
         .then( response => {
           this.gameBoard = response.data  // PUT response contains new AI move
